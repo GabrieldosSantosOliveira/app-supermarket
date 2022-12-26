6389fa7c7bba5afd34a9cae106ac6767b02eb388
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 19:13:25 -0400

    feat: Add component of StatusBar

---
+++ src/components/StatusBar.tsx
@@ -0,0 +1,22 @@
+import { StatusBar as NativeBaseStatusBar } from 'native-base';
+import { FC } from 'react';
+import { StatusBarProps } from 'react-native';
+
+import { useTheme } from '../hooks/useTheme';
+export const StatusBar: FC<StatusBarProps> = ({
+  ...props
+}) => {
+  const { colorMode } = useTheme();
+  return (
+    <NativeBaseStatusBar
+      translucent
+      backgroundColor="transparent"
+      barStyle={
+        colorMode === 'dark'
+          ? 'light-content'
+          : 'dark-content'
+      }
+      {...props}
+    />
+  );
+};
