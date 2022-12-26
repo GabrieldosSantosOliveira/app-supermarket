717dcb54a599d91c40f3748ae86ddba1369f8465
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 19:18:33 -0400

    feat: Add context ThemeContext for UI theme(dark/light)

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,56 @@
+import {
+  IColorModeContextProps,
+  useColorMode
+} from 'native-base';
+import {
+  createContext,
+  FC,
+  ReactNode,
+  useEffect
+} from 'react';
+import { useColorScheme } from 'react-native';
+
+import { setStorage } from '../services/setStorage';
+import { getStorage } from './../services/getStorage';
+type IThemeContext = IColorModeContextProps;
+export const ThemeContext = createContext(
+  {} as IThemeContext
+);
+interface IThemeProviderProps {
+  children: ReactNode;
+}
+export const ThemeProvider: FC<IThemeProviderProps> = ({
+  children
+}) => {
+  const colorScheme = useColorScheme();
+  const { setColorMode, ...rest } = useColorMode();
+  useEffect(() => {
+    async function getInterfaceColor() {
+      const colorSave = await getStorage<
+        'dark' | 'light' | 'automatic'
+      >('@colorMode');
+      if (!colorScheme || colorSave === 'automatic') {
+        if (colorScheme === 'dark') {
+          setColorMode('dark');
+        } else {
+          setColorMode('light');
+        }
+      } else {
+        if (colorSave === 'dark') {
+          setColorMode('dark');
+        } else {
+          setColorMode('light');
+        }
+      }
+    }
+    getInterfaceColor();
+  }, [colorScheme]);
+
+  return (
+    <ThemeContext.Provider
+      value={{ setColorMode, ...rest }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
