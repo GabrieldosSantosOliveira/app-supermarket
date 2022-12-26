332fd4b8e1a474026bdc92175d6ae1add258b2b4
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 19:01:52 -0400

    feat: Add native-base, AuthProvider and custom fonts

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import { NativeBaseProvider } from 'native-base';
+
+import { Loading } from './src/components/Loading';
+import { StatusBar } from './src/components/StatusBar';
+import { AuthProvider } from './src/context/AuthContext';
+import { ThemeProvider } from './src/context/ThemeContext';
+import { Routes } from './src/routes';
+import { THEME } from './src/styles/theme';
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
   return (
-    <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+    <NativeBaseProvider theme={THEME}>
+      <ThemeProvider>
+        <AuthProvider>
+          <StatusBar />
+          {fontsLoaded ? <Routes /> : <Loading />}
+        </AuthProvider>
+      </ThemeProvider>
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
