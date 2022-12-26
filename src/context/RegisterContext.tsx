5091b1d28a3d21018dad0ed3716bf458cb0728f9
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 19:20:52 -0400

    feat: Add context RegisterContext to the multiscreen form

---
+++ src/context/RegisterContext.tsx
@@ -0,0 +1,14 @@
+import { createContext, FC, ReactNode } from 'react';
+export const RegisterContext = createContext({});
+interface IRegisterProvider {
+  children: ReactNode;
+}
+export const RegisterProvider: FC<IRegisterProvider> = ({
+  children
+}) => {
+  return (
+    <RegisterContext.Provider value={{}}>
+      {children}
+    </RegisterContext.Provider>
+  );
+};
